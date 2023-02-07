89eb84b62385b402521e5eb43905000d6e8929f0
Tristan Tarnowski <5461450+tristantarnowski@users.noreply.github.com>
2023-02-07 15:26:58 -0800

    Add HorizontalTable component

---
+++ src/components/HorizontalTable.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let objects: { header: string; data?: (string | number)[] }[];
+</script>
+
+<table>
+    <tbody>
+        {#each objects as object}
+            <tr>
+                <th>{object.header}</th>
+                {#if object.data}
+                    {#each object.data as data}
+                        <td>{data}</td>
+                    {/each}
+                {/if}
+            </tr>
+        {/each}
+    </tbody>
+</table>
--- src/components/InfoTable.svelte
+++ src/components/InfoTable.svelte
@@ -1,53 +1,50 @@
 <script lang="ts">
     import type { Satellite } from "../passtime/passtime";
     import * as satellite from "satellite.js";
+    import HorizontalTable from "./HorizontalTable.svelte";
 
     export let sat: Satellite;
 
     let tleAge = 0,
         period = null;
 
-    let rows = new Map();
-    rows.set("Name", []);
-    rows.set("Catalog Number", []);
-    rows.set("Epoch", []);
-    rows.set("TLE Age", []);
-    rows.set("Eccentricity", []);
-    rows.set("Inclination", []);
-    rows.set("RAAN", []);
-    rows.set("Arg of Perigee", []);
-    rows.set("Mean Anomaly", []);
-    rows.set("B*", []);
+    function getObjects(sat?: Satellite) {
+        const objects = [
+            { header: "Name", data: [sat ? sat.name : ""] },
+            { header: "Catalog Number", data: [sat ? sat.satrec.satnum : ""] },
+            { header: "Epoch", data: [sat ? sat.epochDate.toISOString() : ""] },
+            { header: "TLE Age", data: [sat ? (tleAge / 86400000).toFixed(2) + " days" : ""] },
+            { header: "Eccentricity", data: [sat ? sat.satrec.ecco : ""] },
+            {
+                header: "Inclination",
+                data: [sat ? satellite.radiansToDegrees(sat.satrec.inclo) + " degrees" : ""],
+            },
+            {
+                header: "RAAN",
+                data: [sat ? satellite.radiansToDegrees(sat.satrec.nodeo) + " degrees" : ""],
+            },
+            {
+                header: "Arg of Perigee",
+                data: [sat ? satellite.radiansToDegrees(sat.satrec.argpo) + " degrees" : ""],
+            },
+            {
+                header: "Mean Anomaly",
+                data: [sat ? satellite.radiansToDegrees(sat.satrec.mo) + " degrees" : ""],
+            },
+            {
+                header: "B*",
+                data: [sat ? sat.satrec.bstar : ""],
+            },
+        ];
+        return objects;
+    }
 
     $: if (sat) {
         tleAge = Date.now() - sat.epochDate.getTime();
-
-        rows.set("Name", [sat.name]);
-        rows.set("Catalog Number", [sat.satrec.satnum]);
-        rows.set("Epoch", [sat.epochDate.toISOString()]);
-        rows.set("TLE Age", [(tleAge / 86400000).toFixed(2) + " days"]);
-        rows.set("Eccentricity", [sat.satrec.ecco]);
-        rows.set("Inclination", [satellite.radiansToDegrees(sat.satrec.inclo) + " degrees"]);
-        rows.set("RAAN", [satellite.radiansToDegrees(sat.satrec.nodeo) + " degrees"]);
-        rows.set("Arg of Perigee", [satellite.radiansToDegrees(sat.satrec.argpo) + " degrees"]);
-        rows.set("Mean Anomaly", [satellite.radiansToDegrees(sat.satrec.mo) + " degrees"]);
-        rows.set("B*", [sat.satrec.bstar]);
-        rows = rows;
     }
 </script>
 
-<table>
-    <tbody>
-        {#each [...rows] as [key, value]}
-            <tr>
-                <th>{key}</th>
-                {#each value as data}
-                    <td>{data}</td>
-                {/each}
-            </tr>
-        {/each}
-    </tbody>
-</table>
+<HorizontalTable objects={getObjects(sat)} />
 
 <style>
     tbody,
