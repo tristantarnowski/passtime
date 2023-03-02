31432791568ac6fcf50ca9c57e444d7916033800
Tristan Tarnowski <5461450+tristantarnowski@users.noreply.github.com>
2023-03-01 22:05:01 -0800

    Add missing table fields

--- src/components/InfoTable.svelte
+++ src/components/InfoTable.svelte
@@ -4,11 +4,15 @@
 
     export let sat: Satellite;
 
-    let tleAge = 0,
-        period = null;
+    const earthRadius = 6378.135e3; // meters. WGS72 equatorial radius
+
+    let tleAge, meanMotion, period;
 
     $: if (sat) {
         tleAge = Date.now() - sat.epochDate.getTime();
+
+        meanMotion = (sat.satrec.no_kozai / 2 / Math.PI) * 24 * 60; // rev/day
+        period = 86400 / meanMotion; // seconds
     }
 </script>
 
@@ -18,28 +22,50 @@
         <tr><th>Catalog Number</th><td>{sat ? sat.satrec.satnum : ""}</td></tr>
         <tr><th>Epoch</th><td>{sat ? sat.epochDate.toISOString() : ""}</td></tr>
         <tr><th>TLE Age</th><td>{sat ? (tleAge / 86400000).toFixed(2) + " days" : ""}</td></tr>
-        <!-- <tr><th>Mean Motion</th><td>{sat.satrec.no}</td></tr> -->
-        <!-- <tr><th>Orbital Period</th><td>{period}</td></tr> -->
-        <!-- <tr><th>Semi-Major Axis</th><td>{sat.satrec.a}</td></tr> -->
-        <tr><th>Eccentricity</th><td>{sat ? sat.satrec.ecco : ""}</td></tr>
-        <!-- <tr><th>Apogee Height</th><td>{sat.satrec.a}</td></tr> -->
-        <!-- <tr><th>Perigee Height</th><td>{sat.satrec.satnum}</td></tr> -->
+        <tr><th>Mean Motion</th><td>{sat ? meanMotion.toFixed(8) : ""}</td></tr>
+        <tr><th>Orbital Period</th><td>{sat ? (period / 60).toFixed(2) : ""}</td></tr>
+        <tr
+            ><th>Semi-Major Axis</th><td
+                >{sat ? ((sat.satrec.a * earthRadius) / 1000).toFixed(3) : ""}</td
+            ></tr
+        >
+        <tr><th>Eccentricity</th><td>{sat ? sat.satrec.ecco.toFixed(7) : ""}</td></tr>
+        <tr
+            ><th>Apogee Height</th><td
+                >{sat ? ((sat.satrec.alta * earthRadius) / 1000).toFixed(3) : ""}</td
+            ></tr
+        >
+        <tr
+            ><th>Perigee Height</th><td
+                >{sat ? ((sat.satrec.altp * earthRadius) / 1000).toFixed(3) : ""}</td
+            ></tr
+        >
         <tr>
             <th>Inclination</th>
-            <td>{sat ? satellite.radiansToDegrees(sat.satrec.inclo) + " degrees" : ""}</td>
+            <td
+                >{sat
+                    ? satellite.radiansToDegrees(sat.satrec.inclo).toFixed(4) + " degrees"
+                    : ""}</td
+            >
         </tr>
         <tr
             ><th>RAAN</th><td
-                >{sat ? satellite.radiansToDegrees(sat.satrec.nodeo) + " degrees" : ""}</td
+                >{sat
+                    ? satellite.radiansToDegrees(sat.satrec.nodeo).toFixed(4) + " degrees"
+                    : ""}</td
             ></tr
         >
         <tr>
             <th>Arg of Perigee</th>
-            <td>{sat ? satellite.radiansToDegrees(sat.satrec.argpo) + " degrees" : ""}</td>
+            <td
+                >{sat
+                    ? satellite.radiansToDegrees(sat.satrec.argpo).toFixed(4) + " degrees"
+                    : ""}</td
+            >
         </tr>
         <tr>
             <th>Mean Anomaly</th>
-            <td>{sat ? satellite.radiansToDegrees(sat.satrec.mo) + " degrees" : ""}</td>
+            <td>{sat ? satellite.radiansToDegrees(sat.satrec.mo).toFixed(4) + " degrees" : ""}</td>
         </tr>
         <tr><th>B*</th><td>{sat ? sat.satrec.bstar : ""}</td></tr>
     </tbody>
